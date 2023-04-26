8364fe7d4f0b9fd37323130fa5f14ebadef6eb46
annabusaga <anna.busaga@gmail.com>
2023-04-26 21:07:45 +0300

    commit

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,4 +1,4 @@
-import css from './Notification.module.css';
+// import css from './Notification.module.css';
 
 import React from 'react';
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App/App';
 
-import './index.css';
+// import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
