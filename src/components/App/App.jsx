a48257290b98e8831a0c1d418afa9897aa037f8e
annabusaga <anna.busaga@gmail.com>
2023-04-27 13:38:15 +0300

    commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -22,7 +22,7 @@ export class App extends Component {
     return good + neutral + bad;
   };
   countPositiveFeedbackPercentage = () => {
-    const { good, neutral, bad } = this.state;
+    const { good } = this.state;
     return `${Math.round((good / this.countTotalFeedback()) * 100)}%`;
   };
 
